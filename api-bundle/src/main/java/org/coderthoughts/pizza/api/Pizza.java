bb7ea3556909e28d57c3c6cb2c8afc809ff19769
David Bosschaert <bosschae@adobe.com>
2015-05-28 11:44:06 +0200

    wip

--- api-bundle/src/main/java/org/coderthoughts/pizza/api/Pizza.java
+++ api-bundle/src/main/java/org/coderthoughts/pizza/api/Pizza.java
@@ -1,8 +1,23 @@
+/*************************************************************************
+ *
+ * ADOBE CONFIDENTIAL
+ * __________________
+ *
+ *  Copyright 2014 Adobe Systems Incorporated
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Adobe Systems Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Adobe Systems Incorporated and its
+ * suppliers and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe Systems Incorporated.
+ **************************************************************************/
 package org.coderthoughts.pizza.api;
 
-import org.osgi.annotation.versioning.ProviderType;
 
-@ProviderType
 public interface Pizza {
     String getName();
 }
--- svc-bundle/src/main/java/org/coderthoughts/pizza/svc/Activator.java
+++ svc-bundle/src/main/java/org/coderthoughts/pizza/svc/Activator.java
@@ -1,3 +1,20 @@
+/*************************************************************************
+ *
+ * ADOBE CONFIDENTIAL
+ * __________________
+ *
+ *  Copyright 2014 Adobe Systems Incorporated
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Adobe Systems Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Adobe Systems Incorporated and its
+ * suppliers and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe Systems Incorporated.
+ **************************************************************************/
 package org.coderthoughts.pizza.svc;
 
 import java.util.Dictionary;
@@ -10,7 +27,7 @@ import org.osgi.framework.BundleContext;
 public class Activator implements BundleActivator {
     @Override
     public void start(BundleContext context) throws Exception {
-        Dictionary<String, Object> props = new Hashtable<>();
+        Dictionary<String, Object> props = new Hashtable<String, Object>();
         props.put("toppings", 4);
         context.registerService(Pizza.class, new QuattroStagioni(), props);
     }
